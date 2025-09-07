62c52f97417e6ad382d5508ef2e7d877d6dc557e
guanyuanpeterpan <guanyuanpeterpan@gmail.com>
2025-09-07 13:27:01 +0000

    Deploying to gh-pages from @ guanyuanpeterpan/guanyuanpeterpan.github.io@1f1df280d2ec630af9e69508143088f7657582fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,15 +418,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "projects-project-1",
+            },},{id: "news-our-work-gnn-aclp-graph-neural-networks-based-analog-circuit-link-prediction-is-released-on-arxiv",
+          title: 'Our work GNN-ACLP: Graph Neural Networks Based Analog Circuit Link Prediction is released...',
+          description: "",
+          section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
