4b4e897c1f0e34f4a4107f84aeb691f1e9881fc8
guanyuanpeterpan <guanyuanpeterpan@gmail.com>
2026-05-07 13:20:01 +0000

    Deploying to gh-pages from @ guanyuanpeterpan/guanyuanpeterpan.github.io@43f8643a70a499f872d58ba1777a16a3bede06e5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,58 +46,15 @@ ninja.data = [{
           section: "News",},{id: "news-the-work-graph-neural-networks-based-analog-circuit-link-prediction-is-published-on-engineering-applications-of-artificial-intelligence",
           title: 'The work Graph Neural Networks Based Analog Circuit Link Prediction is published on...',
           description: "",
-          section: "News",},{id: "news-project-edge-deployable-dual-arm-manipulation-algorithms-won-the-national-second-prize-in-the-ai-special-competition-of-the-19th-challenge-cup-national-college-students-extracurricular-academic-science-and-technology-contest",
-          title: 'Project “Edge-Deployable Dual-Arm Manipulation Algorithms” won the National Second Prize in the “AI+”...',
+          section: "News",},{id: "news-announcement-4",
+          title: 'Announcement_4',
           description: "",
-          section: "News",},{id: "news-the-work-vlm-cad-vlm-optimized-collaborative-agent-design-workflow-for-analog-circuit-sizing-is-released-on-arxiv",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_4/";
+            },},{id: "news-the-work-vlm-cad-vlm-optimized-collaborative-agent-design-workflow-for-analog-circuit-sizing-is-released-on-arxiv",
           title: 'The work VLM-CAD: VLM-Optimized Collaborative Agent Design Workflow for Analog Circuit Sizing is...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-ielts-writing-llm",
+          section: "News",},{id: "projects-ielts-writing-llm",
           title: 'IELTS Writing LLM',
           description: "An LLM-powered tool for IELTS Writing practice and evaluation.",
           section: "Projects",handler: () => {
